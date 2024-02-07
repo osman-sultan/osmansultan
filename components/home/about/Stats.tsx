7c679b96bd0b04e20e360bfdd7b4e912d3f9e014
Osman Sultan <osmansultan2002@gmail.com>
2024-02-06 21:09:36 -0500

    update skills

--- components/home/about/Stats.tsx
+++ components/home/about/Stats.tsx
@@ -14,11 +14,9 @@ export const Stats = () => {
           </h4>
           <div className={styles.statGrid}>
             <span className="chip">Python</span>
-            <span className="chip">JavaScript</span>
             <span className="chip">TypeScript</span>
             <span className="chip">Java</span>
             <span className="chip">SQL</span>
-            <span className="chip">C++</span>
           </div>
         </div>
       </Reveal>
@@ -27,16 +25,16 @@ export const Stats = () => {
         <div className={styles.statColumn}>
           <h4>
             <FaCode size="2.4rem" color="var(--brand)" />
-            <span>Frameworks</span>
+            <span>Web</span>
           </h4>
           <div className={styles.statGrid}>
             <span className="chip">React.js</span>
-            <span className="chip">Vue.js</span>
             <span className="chip">Next.js</span>
+            <span className="chip">Vue.js</span>
             <span className="chip">Node.js</span>
-            <span className="chip">Express.js</span>
             <span className="chip">Spring Boot</span>
             <span className="chip">FastAPI</span>
+            <span className="chip">TailwindCSS</span>
           </div>
         </div>
       </Reveal>
@@ -45,43 +43,14 @@ export const Stats = () => {
         <div className={styles.statColumn}>
           <h4>
             <FaToolbox size="2.4rem" color="var(--brand)" />
-            <span>ML and Data Analysis</span>
+            <span>Machine Learning</span>
           </h4>
           <div className={styles.statGrid}>
-            <span className="chip">NumPy</span>
-            <span className="chip">Pandas</span>
-            <span className="chip">Matplotlib</span>
-            <span className="chip">Seaborn</span>
+            <span className="chip">NumPy/Pandas</span>
+            <span className="chip">Matplotlib/Seaborn</span>
             <span className="chip">Scikit-learn</span>
             <span className="chip">PyTorch</span>
             <span className="chip">Langchain</span>
-            <span className="chip">Hugging Face</span>
-            <span className="chip">Replicate</span>
-            <span className="chip">PineconeDB</span>
-          </div>
-        </div>
-      </Reveal>
-
-      <Reveal>
-        <div className={styles.statColumn}>
-          <h4>
-            <AiFillCode size="2.4rem" color="var(--brand)" />
-            <span>Web tools</span>
-          </h4>
-          <div className={styles.statGrid}>
-            <span className="chip">TailwindCSS</span>
-            <span className="chip">Bootstrap5</span>
-            <span className="chip">ShadCN UI</span>
-            <span className="chip">Vite</span>
-            <span className="chip">NextAuth.js</span>
-            <span className="chip">Clerk</span>
-            <span className="chip">ESLint</span>
-            <span className="chip">Prisma</span>
-            <span className="chip">Zustand</span>
-            <span className="chip">Zod</span>
-            <span className="chip">Tanstack Query</span>
-            <span className="chip">Storybook</span>
-            <span className="chip">Postman</span>
           </div>
         </div>
       </Reveal>
@@ -93,15 +62,13 @@ export const Stats = () => {
             <span>Infrastructure</span>
           </h4>
           <div className={styles.statGrid}>
-            <span className="chip">MongoDB</span>
-            <span className="chip">Firebase</span>
-            <span className="chip">Supabase</span>
-            <span className="chip">AWS S3</span>
             <span className="chip">Redis</span>
-            <span className="chip">SQL</span>
             <span className="chip">SQLite</span>
             <span className="chip">MySQL</span>
             <span className="chip">PostgreSQL</span>
+            <span className="chip">Supabase</span>
+            <span className="chip">MongoDB</span>
+            <span className="chip">AWS S3/Lambda</span>
             <span className="chip">Vercel</span>
           </div>
         </div>
@@ -115,14 +82,11 @@ export const Stats = () => {
           </h4>
           <div className={styles.statGrid}>
             <span className="chip">Git</span>
-            <span className="chip">GitHub/GitLab</span>
             <span className="chip">Bash</span>
-            <span className="chip">PowerShell</span>
             <span className="chip">Linux</span>
-            <span className="chip">Ubuntu</span>
             <span className="chip">Docker</span>
-            <span className="chip">WSL</span>
             <span className="chip">Ngrok</span>
+            <span className="chip">Postman</span>
           </div>
         </div>
       </Reveal>
